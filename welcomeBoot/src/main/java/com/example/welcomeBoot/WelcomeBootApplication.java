ac24c1d9755d2c5d524f4ec7481e9279b3ace51e
JAE <jack3787@naver.com>
2022-02-24 18:06:08 +0900

    welcomeBoot

---
+++ welcomeBoot/src/main/java/com/example/welcomeBoot/WelcomeBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.welcomeBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeBootApplication.class, args);
+	}
+
+}
