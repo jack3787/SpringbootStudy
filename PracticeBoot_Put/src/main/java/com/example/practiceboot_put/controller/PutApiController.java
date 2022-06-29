4beaec4cb5f0eafc7aa0a5cd79e73356e3fbfaf6
JAE <jack3787@naver.com>
2022-06-29 17:57:43 +0900

    [modify] PutApiController

--- PracticeBoot_Put/src/main/java/com/example/practiceboot_put/controller/PutApiController.java
+++ PracticeBoot_Put/src/main/java/com/example/practiceboot_put/controller/PutApiController.java
@@ -1,19 +1,16 @@
 package com.example.practiceboot_put.controller;
 
 import com.example.practiceboot_put.dto.PostRequestDto;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class PutApiController {
 
-    @PutMapping("/put")
-    public PostRequestDto put(@RequestBody PostRequestDto requestDto_put){
-        System.out.println(requestDto_put);
-        return requestDto_put;
+    @PutMapping("/put/{userId}")
+    public PostRequestDto put(@RequestBody PostRequestDto requestDto, @PathVariable(name = "userId") Long id){
+        System.out.println(id);
+        return requestDto;
     }
 
 }
