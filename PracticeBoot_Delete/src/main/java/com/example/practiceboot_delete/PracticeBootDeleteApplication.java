2d678acfadc1bbeed702a8c16a9377c6af808844
JAE <jack3787@naver.com>
2022-06-29 18:28:20 +0900

    [New] Practice SpringBoot Delete

---
+++ PracticeBoot_Delete/src/main/java/com/example/practiceboot_delete/PracticeBootDeleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceboot_delete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeBootDeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeBootDeleteApplication.class, args);
+    }
+
+}
---
+++ PracticeBoot_Delete/src/test/java/com/example/practiceboot_delete/PracticeBootDeleteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practiceboot_delete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeBootDeleteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
